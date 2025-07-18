0a9d7bb70b1d1dfdd609d4578179844eee3f9f91
Francesco Fico <Francesco.Fico@nttdata.com>
2025-07-18 18:42:09 +0200

    feat: [UPBE-23] verify requirements page (#4)
    * VerifyRequirements page + refactor routing
    
    * refactor routes
    
    * init useTranslation with i18n

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,13 @@ import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout/Layout';
 import Dashboard from './pages/Dashboard/Dashboard';
+import VerifyRequirements from './pages/VerifyRequirements/VerifyRequirements';
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route path="utente" element={<Dashboard />} />
-      </Route>
+      <Route path="/utente/dashboard" element={<Layout hasSidebar={true}><Dashboard/></Layout>} />
+      <Route path="/utente/verifyRequirements" element={<Layout hasSidebar={false}><VerifyRequirements/></Layout>} />
     </Routes>
   );
 }
