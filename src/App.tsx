75f0f595e9f367e0a95f270506b2964df0093806
Francesco Fico <Francesco.Fico@nttdata.com>
2025-07-21 14:40:02 +0200

    fix/upbe-23--routes-and-i18n-resource (#5)
    * VerifyRequirements page + refactor routing
    
    * refactor routes
    
    * init useTranslation with i18n
    
    * fix rotte + i18n
    
    * added error logic + fix default route
    
    * fix i18n resource file and colors from mui-italia

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,19 @@ import { Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout/Layout';
 import Dashboard from './pages/Dashboard/Dashboard';
 import VerifyRequirements from './pages/VerifyRequirements/VerifyRequirements';
+import ROUTES from './routes';
+
+const LocalRoutes = () => (
+  <Routes>
+    <Route path={ROUTES.DASHBOARD} element={<Layout hasSidebar={true}><Dashboard/></Layout>} />
+    <Route path={ROUTES.VERIFY_REQUIREMENTS} element={<Layout hasSidebar={false}><VerifyRequirements/></Layout>} />
+  </Routes>
+);
 
 function App() {
   return (
-    <Routes>
-      <Route path="/utente/dashboard" element={<Layout hasSidebar={true}><Dashboard/></Layout>} />
-      <Route path="/utente/verifyRequirements" element={<Layout hasSidebar={false}><VerifyRequirements/></Layout>} />
-    </Routes>
-  );
+		<LocalRoutes />
+	);
 }
 
 export default App;
--- src/locale/it.ts
+++ src/locale/it.ts
@@ -1,37 +1,45 @@
 const it = {
-  "verifyRequirements.exit": "Esci",
-
-  "verifyRequirements.title": "Verifica dei requisiti",
-  "verifyRequirements.description":
-    "Compila i campi e invia la tua richiesta. Se rispetta i requisiti, riceverai il buono sconto da scaricare e mostrare nei punti vendita abilitati.",
-
-  "verifyRequirements.family.title": "Nucleo familiare",
-  "verifyRequirements.family.description1":
-    "Per accedere all’iniziativa {{Bonus Elettrodomestici}}, verificheremo con alcuni enti se hai il requisito relativo al tuo nucleo familiare.",
-  "verifyRequirements.family.description2":
-    "Questo passaggio è richiesto dal Ministero dell’Interno.",
-
-  "verifyRequirements.selfDeclaration.title": "Autodichiarazione",
-  "verifyRequirements.selfDeclaration.description":
-    "L'autodichiarazione è resa ai sensi del D.P.R. 28 dicembre 2000, n. 445.",
-  "verifyRequirements.selfDeclaration.switchLabel":
-    "Dichiaro che userò il bonus per l’acquisto di un elettrodomestico di classe energetica superiore destinato a sostituire un altro della stessa tipologia",
-
-  "verifyRequirements.tooltip":
-    "Comunichiamo direttamente con l’ente che detiene l’informazione necessaria a effettuare il controllo, in questo caso **{{provider}}**. In questo modo non devi produrre alcuna autodichiarazione.",
-
-  "verifyRequirements.isee.title": "Dichiarazione ISEE 2025",
-  "verifyRequirements.isee.description":
-    "Se sei in possesso di un ISEE 2025 inferiore a 25.000€, verrà effettuata una verifica con INPS",
-  "verifyRequirements.isee.option.<25000": "Ho un ISEE inferiore a 25.000€",
-  "verifyRequirements.isee.option.>=25000": "Ho un ISEE uguale o superiore a 25.000€",
-  "verifyRequirements.isee.option.none": "Non ho un ISEE",
-  "verifyRequirements.isee.hint.<25000": "Hai diritto fino a €200",
-  "verifyRequirements.isee.hint.>=25000": "Hai diritto fino a €100",
-  "verifyRequirements.isee.hint.none": "Hai diritto fino a €100",
-
-  "verifyRequirements.back": "Indietro",
-  "verifyRequirements.submit": "Invia richiesta"
+  "verifyRequirements":{
+    "exit": "Esci",
+    "title": "Verifica dei requisiti",
+    "description":
+      "Compila i campi e invia la tua richiesta. Se rispetta i requisiti, riceverai il buono sconto da scaricare e mostrare nei punti vendita abilitati.",
+  
+    "family":{
+      "title": "Nucleo familiare",
+      "description1":
+        "Per accedere all’iniziativa {{Bonus Elettrodomestici}}, verificheremo con alcuni enti se hai il requisito relativo al tuo nucleo familiare.",
+      "description2":
+        "Questo passaggio è richiesto dal Ministero dell’Interno.",
+    },
+  
+    "selfDeclaration":{
+      "title": "Autodichiarazione",
+      "description":
+        "L'autodichiarazione è resa ai sensi del D.P.R. 28 dicembre 2000, n. 445.",
+      "switchLabel":
+        "Dichiaro che userò il bonus per l’acquisto di un elettrodomestico di classe energetica superiore destinato a sostituire un altro della stessa tipologia",
+    },
+  
+    "tooltip":
+      "Comunichiamo direttamente con l’ente che detiene l’informazione necessaria a effettuare il controllo, in questo caso **{{provider}}**. In questo modo non devi produrre alcuna autodichiarazione.",
+    
+    "isee":{
+      "title": "Dichiarazione ISEE 2025",
+      "description":
+        "Se sei in possesso di un ISEE 2025 inferiore a 25.000€, verrà effettuata una verifica con INPS",
+      "option.<25000": "Ho un ISEE inferiore a 25.000€",
+      "option.>=25000": "Ho un ISEE uguale o superiore a 25.000€",
+      "option.none": "Non ho un ISEE",
+      "hint.<25000": "Hai diritto fino a €200",
+      "hint.>=25000": "Hai diritto fino a €100",
+      "hint.none": "Hai diritto fino a €100",
+    },
+    
+    "error": "Seleziona un’opzione per continuare",
+    "back": "Indietro",
+    "submit": "Invia richiesta"
+  },
 };
 
 export default it;
