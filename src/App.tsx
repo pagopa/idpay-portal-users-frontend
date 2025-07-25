dfa613b921de5f65ecf3ddbb1a2d8a121e83b37a
abilello1 <alessio.bilello@emeal.nttdata.com>
2025-07-25 10:05:41 +0200

    add route INSERT_EMAIL

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,21 @@ import { Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout/Layout';
 import Dashboard from './pages/Dashboard/Dashboard';
 import VerifyRequirements from './pages/VerifyRequirements/VerifyRequirements';
+import InsertEmail from './pages/InsertEmail/InsertEmail';
 import ROUTES from './routes';
 
 const LocalRoutes = () => (
   <Routes>
     <Route path={ROUTES.DASHBOARD} element={<Layout hasSidebar={true}><Dashboard/></Layout>} />
     <Route path={ROUTES.VERIFY_REQUIREMENTS} element={<Layout hasSidebar={false}><VerifyRequirements/></Layout>} />
+    <Route path={ROUTES.INSERT_EMAIL} element={<Layout hasSidebar={false}><InsertEmail /></Layout>} />
   </Routes>
 );
 
 function App() {
   return (
-		<LocalRoutes />
-	);
+    <LocalRoutes />
+  );
 }
 
 export default App;
