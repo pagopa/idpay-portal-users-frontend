ec9eb3fe1ee13f83f54b3dcf95a7c410f5e4aa79
abilello1 <164199664+abilello1@users.noreply.github.com>
2025-07-28 16:14:14 +0200

    feat: [UPBE-24] email input validation (#10)
    * update gitIngore and jest setup
    
    * update i18n labels
    
    * add route INSERT_EMAIL
    
    * update App test
    
    * add EmailInputBox
    
    * add InsertEmail
    
    * update VerifyRequirements
    
    * add tests
    
    * update code-review
    
    * fix
    
    * improve email validation
    
    * minor refactor validation email
    
    * add TitleCard test
    
    * update code-review yml
    
    * fix code-review yml
    
    * improve InsertEmail layout

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -1,48 +1,12 @@
 import { render } from '@testing-library/react';
-import { useAuth } from 'react-oidc-context';
+import { MemoryRouter } from 'react-router-dom';
 import App from '../App';
+import ROUTES from '../routes';
 
-jest.mock("react-i18next", () => ({
-  useTranslation: () => ({
-    t: (key: string) => key,
-    i18n: { changeLanguage: () => new Promise(() => {}) }
-  })
-}));
-
-jest.mock('react-oidc-context', () => ({
-  useAuth: jest.fn(),
-}));
-
-describe('App component', () => {
-//   it('renders loading', () => {
-//     (useAuth as jest.Mock).mockReturnValue({
-//       isLoading: true,
-//       isAuthenticated: false,
-//       signinRedirect: jest.fn(),
-//     });
-
-//     render(<App />);
-//     expect(screen.getByText(/Caricamento sessione/i)).toBeInTheDocument();
-//   });
-
-//   it('renders redirect message', () => {
-//     (useAuth as jest.Mock).mockReturnValue({
-//       isLoading: false,
-//       isAuthenticated: false,
-//       signinRedirect: jest.fn(),
-//     });
-
-//     render(<App />);
-//     expect(screen.getByText(/Reindirizzamento al login/i)).toBeInTheDocument();
-//   });
-
-  it('renders layout when authenticated', () => {
-    (useAuth as jest.Mock).mockReturnValue({
-      isLoading: false,
-      isAuthenticated: true,
-      signinRedirect: jest.fn(),
-    });
-
-    render(<App />);
-  });
+test('renders App component without crashing', () => {
+  render(
+    <MemoryRouter initialEntries={[ROUTES.DASHBOARD]}>
+      <App />
+    </MemoryRouter>
+  );
 });
