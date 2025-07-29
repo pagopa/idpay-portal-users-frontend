b63642830d632d60b6e8283ddb8839c5964ff989
abilello1 <alessio.bilello@emeal.nttdata.com>
2025-07-29 11:53:23 +0200

    add path and route for FeedbackPage

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Layout from './components/Layout/Layout';
 import Dashboard from './pages/Dashboard/Dashboard';
 import VerifyRequirements from './pages/VerifyRequirements/VerifyRequirements';
 import InsertEmail from './pages/InsertEmail/InsertEmail';
+import FeedbackPage from './pages/FeedbackPage/FeedbackPage';
 import ROUTES from './routes';
 
 const LocalRoutes = () => (
@@ -11,6 +12,7 @@ const LocalRoutes = () => (
     <Route path={ROUTES.DASHBOARD} element={<Layout hasSidebar={true}><Dashboard/></Layout>} />
     <Route path={ROUTES.VERIFY_REQUIREMENTS} element={<Layout hasSidebar={false}><VerifyRequirements/></Layout>} />
     <Route path={ROUTES.INSERT_EMAIL} element={<Layout hasSidebar={false}><InsertEmail /></Layout>} />
+    <Route path={ROUTES.FEEDBACK} element={<Layout hasSidebar={false}><FeedbackPage /></Layout>} />
   </Routes>
 );
 
--- src/routes.tsx
+++ src/routes.tsx
@@ -2,7 +2,8 @@
 const ROUTES = {
 	DASHBOARD: "/utente",
     VERIFY_REQUIREMENTS: "/utente/verifica-requisiti",
-    INSERT_EMAIL: "/utente/inserisci-email"
+    INSERT_EMAIL: "/utente/inserisci-email",
+    FEEDBACK: "/utente/esito"
 };
 
 export default ROUTES;
