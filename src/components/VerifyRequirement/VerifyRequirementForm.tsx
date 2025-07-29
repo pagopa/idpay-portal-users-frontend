cdfc28ab130193005da97989b2dfc18c0842df9d
abilello1 <alessio.bilello@emeal.nttdata.com>
2025-07-29 18:16:30 +0200

    add page navigation

--- src/components/VerifyRequirement/VerifyRequirementForm.tsx
+++ src/components/VerifyRequirement/VerifyRequirementForm.tsx
@@ -8,11 +8,26 @@ import FamilyForm from './FamilyForm';
 import IseeForm from './IseeForm';
 import SelfDeclaration from './SelfDeclaration';
 import HeaderForm from './HeaderForm';
+import ROUTES from '../../routes';
+import { useNavigate } from 'react-router-dom';
 
 export default function VerifyRequirementForm() {
     const { t } = useTranslation();
+    const navigate = useNavigate();
+
     const [iseeValue, setIseeValue] = useState('');
     const [switchValue, setSwitchValue] = useState(false);
+
+    const handleBack = () => {
+        navigate(ROUTES.INSERT_EMAIL);
+    }
+
+    const handleContinue = () => {
+        //TODO add API call
+        const feedbackStatus = 'REQUEST_SUBMITTED'
+        navigate(ROUTES.FEEDBACK, {state: {status: feedbackStatus}})
+    }
+    
     return (
         <Container sx={{ width: "100%", px: {lg: "5%", md: "20%", sm: "10%", xs: "1%"} }}>
             <Box
@@ -29,8 +44,8 @@ export default function VerifyRequirementForm() {
                 <IseeForm iseeValue={iseeValue} setIseeValue={setIseeValue} />
 
                 <Box sx={{ display: "flex", justifyContent: "space-between" }}>
-                    <Button variant="outlined" size='medium' startIcon={<ArrowBack sx={{ color: theme.palette.primary.main }} />}>{t('verifyRequirements.back')}</Button>
-                    <Button variant="contained" size='medium'>{t('verifyRequirements.submit')}</Button>
+                    <Button variant="outlined" size='medium' startIcon={<ArrowBack sx={{ color: theme.palette.primary.main }} />} onClick={handleBack}>{t('commons.back')}</Button>
+                    <Button variant="contained" size='medium' onClick={handleContinue}>{t('verifyRequirements.submit')}</Button>
                 </Box>
             </Box>
         </Container>
