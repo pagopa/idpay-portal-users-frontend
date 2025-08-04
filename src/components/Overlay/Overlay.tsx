f89edddc7548c40aae5c1204f3189d409f640cda
abilello1 <alessio.bilello@emeal.nttdata.com>
2025-08-04 09:39:18 +0200

    add Overlay

--- src/components/Overlay/Overlay.tsx
+++ src/components/Overlay/Overlay.tsx
@@ -2,15 +2,19 @@ import { Box, CircularProgress } from "@mui/material";
 import { theme } from '@pagopa/mui-italia';
 
 const Overlay = () => (
-    <Box
-        height={'50vh'}
-        display={'flex'}
-        alignItems={'center'}
-        justifyContent={'center'}
-        bgcolor={theme.palette.background.default}
-    >
-        <CircularProgress />
-    </Box>
+  <Box
+    position="fixed"
+    top={0}
+    left={0}
+    width="100vw"
+    height="100vh"
+    display="flex"
+    alignItems="center"
+    justifyContent="center"
+    bgcolor={theme.palette.action.disabledBackground}
+  >
+    <CircularProgress />
+  </Box>
 );
 
 export default Overlay;
