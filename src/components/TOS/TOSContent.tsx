9b8686eed201b0db0571f216873aa3a2757e2a13
Francesco Fico <Francesco.Fico@nttdata.com>
2025-08-04 17:20:19 +0200

    fix: [Upbe-67] ToS design alignment (#20)

--- src/components/TOS/TOSContent.tsx
+++ src/components/TOS/TOSContent.tsx
@@ -1,8 +1,10 @@
-import { Box, Container, Typography, List, ListItem, Link, Button } from '@mui/material';
+import { Box, Container, Typography, List, ListItem, Link, Button, Checkbox, FormControl } from '@mui/material';
 import { Trans, useTranslation } from 'react-i18next';
 import { theme } from '@pagopa/mui-italia';
 import { useNavigate } from 'react-router-dom';
 import ROUTES from '../../routes';
+import { useState } from 'react';
+import { useIsMobile } from '../../hooks/useIsMobile';
 
 interface Props {
   sectionRefs: React.RefObject<HTMLDivElement>[];
@@ -11,10 +13,17 @@ interface Props {
 export const TOSContent = ({ sectionRefs }: Props) => {
   const { t } = useTranslation();
   const navigate = useNavigate();
+  const [checked, setChecked] = useState(false);
+  const [error, setError] = useState(false);
+  const isMobile = useIsMobile();
 
   const handleContinue = () => {
-    //TODO call API
-    navigate(ROUTES.INSERT_EMAIL);
+    if (!checked) {
+      setError(true);
+    } else {
+      //TODO call API
+      navigate(ROUTES.INSERT_EMAIL);
+    }
   }
 
   return (
@@ -79,8 +88,8 @@ export const TOSContent = ({ sectionRefs }: Props) => {
           </ListItem>
         </List>
 
-        <Typography sx={{ color: theme.palette.primary.main, fontWeight: theme.typography.fontWeightMedium, mb: 4 }}>
-          <Link href="" underline="always" target="_blank">
+        <Typography sx={{ color: theme.palette.primary.main, fontWeight: theme.typography.fontWeightMedium, mb: 4, cursor: "pointer" }}>
+          <Link onClick={() => { }} underline="always" target="_blank">
             {t('tos.sideMenu.element2.link')}
           </Link>
         </Typography>
@@ -115,8 +124,8 @@ export const TOSContent = ({ sectionRefs }: Props) => {
           />
         </Typography>
 
-        <Typography sx={{ color: theme.palette.primary.main, fontWeight: theme.typography.fontWeightMedium, mb: 4, mt: 2 }}>
-          <Link href="" underline="always" target="_blank">
+        <Typography sx={{ color: theme.palette.primary.main, fontWeight: theme.typography.fontWeightMedium, mb: 4, mt: 2, cursor: "pointer" }}>
+          <Link onClick={() => { }} underline="always" target="_blank">
             {t('tos.sideMenu.element4.link')}
           </Link>
         </Typography>
@@ -130,17 +139,91 @@ export const TOSContent = ({ sectionRefs }: Props) => {
           />
         </Typography>
 
-        <Typography sx={{ color: theme.palette.text.primary }} mt={5}>
-          <Trans
-            i18nKey="tos.privacy"
-            components={{
-              a: <Box component="span" sx={{ color: theme.palette.primary.main, cursor: 'pointer' }} />,
-            }}
-          />
-        </Typography>
+        <Box mt={5} display="flex" alignItems="center">
+          <FormControl sx={{ mr: isMobile ? 2 : 0 }}>
+            <Checkbox
+              checked={checked}
+              onChange={(e) => {
+                setChecked(e.target.checked);
+                if (e.target.checked) setError(false);
+              }}
+              color="primary" />
+          </FormControl>
+
+          <Box display="flex" flexWrap="wrap" alignItems="center">
+            <Typography
+              variant='body1'
+              sx={{
+                lineHeight: '24px',
+                color: theme.palette.text.secondary,
+              }}
+            >
+              {t('tos.privacy_part1')}
+            </Typography>
+
+            <Link
+              onClick={() => { }}
+              underline="hover"
+              component="button"
+              sx={{
+                lineHeight: '24px',
+                mx: 0.5,
+              }}
+            >
+              <Typography
+                variant='body1'
+                sx={{
+                  lineHeight: '24px',
+                  color: theme.palette.primary.main
+                }}
+              >
+                {t('tos.privacy_terms')}
+              </Typography>
+            </Link>
+
+            <Typography
+              variant='body1'
+              sx={{
+                lineHeight: '24px',
+                color: theme.palette.text.secondary,
+              }}
+            >
+              {t('tos.privacy_part2')}
+            </Typography>
+
+            <Link
+              component="button"
+              onClick={() => { }}
+              underline="hover"
+              sx={{
+                lineHeight: '24px',
+                mx: 0,
+              }}
+            >
+              <Typography
+                variant='body1'
+                sx={{
+                  lineHeight: '24px',
+                  color: theme.palette.primary.main
+                }}
+              >
+                {t('tos.privacy_policy')}
+              </Typography>
+            </Link>
+          </Box>
+        </Box>
+
+        {error && (
+          <Typography
+            variant="caption"
+            sx={{ color: theme.palette.error.main, display: "block", mt: 0.5 }}
+          >
+            {t('commons.mandatoryField')}
+          </Typography>
+        )}
       </Box>
       <Box sx={{ py: 6 }}>
-        <Button variant="outlined" color='error' sx={{ mr: { md: 2, sm: 1, xs: 1 } }}>{t('exit')}</Button>
+        <Button variant="outlined" sx={{ mr: { md: 2, sm: 1, xs: 1 } }}>{t('exit')}</Button>
         <Button variant="contained" onClick={handleContinue}>{t('tos.continue')}</Button>
       </Box>
     </Container>
