b304244e9fa48d68cae54648f123a3a4dd78e786
abilello1 <alessio.bilello@emeal.nttdata.com>
2025-08-08 14:56:56 +0200

    remove unused commented code

--- src/main.tsx
+++ src/main.tsx
@@ -4,27 +4,10 @@ import App from './App.tsx';
 import { CssBaseline, ThemeProvider } from '@mui/material';
 import { theme } from '@pagopa/mui-italia';
 import { BrowserRouter } from 'react-router-dom';
-
 import './locale/i18n';
-// import { AuthProvider } from "react-oidc-context";
-
-
-// const oidcConfig = {
-//   authority: import.meta.env.VITE_OIDC_AUTHORITY, //provider url
-//   client_id: import.meta.env.VITE_OIDC_CLIENT_ID, //client univoque id
-//   redirect_uri: import.meta.env.VITE_OIDC_REDIRECT_URI, //redirect uri after a successful login
-//   automaticSilentRenew: true, // automatically renews tokens
-//   onSigninCallback: () => {
-//     // clean the url after a successful login
-//     window.history.replaceState({}, document.title, window.location.pathname);
-//   },
-// };
-
 import { AuthProvider } from "./contexts/AuthContext";
 
-
 createRoot(document.getElementById('root')!).render(
-  // <AuthProvider {...oidcConfig}>
   <AuthProvider>
     <BrowserRouter>
       <ThemeProvider theme={theme}>
