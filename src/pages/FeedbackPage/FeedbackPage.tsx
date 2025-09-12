00c3e9f3d7d7f819693bde202e35ed23796ae6a8
Francesco Fico <Francesco.Fico@nttdata.com>
2025-09-12 12:26:54 +0200

    fix refactor duplicated lines

--- src/pages/FeedbackPage/FeedbackPage.tsx
+++ src/pages/FeedbackPage/FeedbackPage.tsx
@@ -1,32 +1,5 @@
-import { useLocation, Navigate } from 'react-router-dom';
-import { feedbackStates } from './feedbackStates';
-import FeedbackContent from '../../components/FeedbackContent/FeedbackContent';
-import { useTranslation } from 'react-i18next';
-import { Box } from '@mui/material';
+import { feedbackStates } from "./feedbackStates";
+import { makeStatusPage } from "../StatusPageFactory/StatusPageFactory";
 
-const FeedbackPage = () => {
-  const { t } = useTranslation();
-  const location = useLocation();
-  const status = location.state?.status as keyof typeof feedbackStates;
-  const feedback = feedbackStates[status] || feedbackStates.ON_EVALUATION; //TODO tmp fallback
-
-  if (!feedback) {
-    return <Navigate to="/" replace />;
-  }
-
-  return (
-    <Box sx={{ py: { xs: 4, sm: 6 }, height: "100%", }} justifyContent="center" display="flex" alignItems="center" maxWidth={"sm"} mx="auto" >
-      <FeedbackContent
-        icon={feedback.icon}
-        title={t(feedback.title)}
-        description={t(feedback.description)}
-        buttonLabel={feedback.buttonLabel && t(feedback.buttonLabel)}
-        buttonRedirect={feedback.buttonRedirect}
-        supportLinkLabel={feedback.supportLinkLabel && t(feedback.supportLinkLabel)}
-        supportLinkUrl={feedback.supportLinkUrl}
-      />
-    </Box>
-  );
-};
-
+const FeedbackPage = makeStatusPage(feedbackStates, "ON_EVALUATION");
 export default FeedbackPage;
