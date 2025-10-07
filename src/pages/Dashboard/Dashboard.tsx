5e70335c9094501a4b4a402ddab733a27133554e
Francesco Fico <Francesco.Fico@nttdata.com>
2025-10-07 15:11:50 +0200

    feat: [UPBE-244] Timeline API (#77)

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -10,6 +10,8 @@ import { useAuth } from '../../contexts/AuthContext';
 import DetailBonusCard from '../../components/Dashboard/DetailBonusCard';
 import BarcodeCard from '../../components/Dashboard/BarcodeCard';
 import OperationsCard from '../../components/Dashboard/OperationsCard';
+import { TimelineDTO } from '../../api/generated/onboarding-web/TimelineDTO';
+import { OperationDTO } from '../../api/generated/onboarding-web/OperationDTO';
 
 interface BonusDetail {
   voucherStatus: VoucherStatusEnum;
@@ -18,13 +20,38 @@ interface BonusDetail {
   amountCents: number;
 }
 
+interface TimelineItem {
+  label: string;
+  date: string;
+  cents?: number;
+  id: string;
+}
+
 const Dashboard = () => {
   const { t } = useTranslation();
   const [bonusData, setBonusData] = useState<BonusDetail | null>(null);
+  const [timeline, setTimeline] = useState<OperationDTO[] | null>(null)
+  const [timelineData, setTimelineData] = useState<TimelineItem[] | null>(null)
+  const [timelineDetailData] = useState<OperationDTO | null>(null)
   const [trxCode, setTrxCode] = useState('');
   const [isLoading, setIsLoading] = useState(true);
   const navigate = useNavigate();
   const { user } = useAuth();
+
+  function formatDate(dateString: string): string {
+    const date = new Date(dateString);
+
+    const formatter = new Intl.DateTimeFormat("it-IT", {
+      day: "2-digit",
+      month: "short",
+      year: "numeric",
+      hour: "2-digit",
+      minute: "2-digit",
+      hour12: false,
+    });
+
+    return formatter.format(date).replace(/\./g, "");
+  }
 
   useEffect(() => {
     const fetchData = async () => {
@@ -40,6 +67,30 @@ const Dashboard = () => {
         ) {
           const barcodeResponse = await OnboardingWebApi.getBarCode(initiativeId);
           setTrxCode(barcodeResponse.data?.trxCode || '');
+        }
+
+        const timelineResponse = await OnboardingWebApi.timeline(initiativeId);
+        if(timelineResponse?.status && timelineResponse.status === 200) {
+          const timelineList = (timelineResponse.data as TimelineDTO).operationList as OperationDTO[];
+          const sortedOperations = [...timelineList].sort(
+            (a, b) => new Date(b.operationDate).getTime() - new Date(a.operationDate).getTime()
+          );
+          setTimeline(sortedOperations);
+          const onboarding = sortedOperations?.find(e => e?.operationType === "ONBOARDING")
+          const onboardingItem: TimelineItem = {label: "Adesione iniziativa", date: formatDate(onboarding?.operationDate!), id: onboarding?.operationId!}
+          var operationItems: TimelineItem[] = []
+
+          for (const operation of sortedOperations) {
+            if (operation?.operationType === "TRANSACTION") {
+              operationItems.push({
+                label: operation.businessName ?? "N/A",
+                date: formatDate(operation.operationDate!),
+                cents: operation.accruedCents,
+                id: operation.operationId
+              });
+            }
+          }
+          setTimelineData([...operationItems, onboardingItem]);
         }
         setIsLoading(false);
       } catch {
@@ -60,7 +111,7 @@ const Dashboard = () => {
   const showBarcode =
     (bonusData.voucherStatus === VoucherStatusEnum.ACTIVE ||
       bonusData.voucherStatus === VoucherStatusEnum.EXPIRING) && Boolean(trxCode);
-
+      console.log(timelineData, timelineDetailData, timeline)
   return (
     <>
       <Box>
