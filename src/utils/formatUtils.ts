7231bb8652a48b5e932caffd761880534edb2976
abilello1 <alessio.bilello@emeal.nttdata.com>
2025-10-08 18:26:01 +0200

    move formatDateTime in utils

--- src/utils/formatUtils.ts
+++ src/utils/formatUtils.ts
@@ -3,3 +3,15 @@ export const formatCurrency = (amountCents: number): string =>
 
 export const formatDate = (dateString: string): string =>
   new Date(dateString).toLocaleDateString('it-IT');
+
+export const formatDateTime = (dateString: string): string =>
+  new Intl.DateTimeFormat('it-IT', {
+    day: '2-digit',
+    month: 'short',
+    year: 'numeric',
+    hour: '2-digit',
+    minute: '2-digit',
+    hour12: false,
+  })
+    .format(new Date(dateString))
+    .replace(/\./g, '');
