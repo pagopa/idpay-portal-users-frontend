c07861f65872aea43696c3926a0a28363ddbe303
abilello1 <alessio.bilello@emeal.nttdata.com>
2025-10-08 18:27:25 +0200

    update i18n labels

--- src/locale/it.ts
+++ src/locale/it.ts
@@ -193,7 +193,7 @@ const it = {
       "title": "Non è possibile\ncompletare la richiesta\nper questa iniziativa",
       "description": "Siamo già a lavoro per risolvere.\nRiprova tra un po’."
     },
-     "tooManyRequests": {
+    "tooManyRequests": {
       "title": "Sembra che le cose\nvadano un po' a rilento",
       "description": "Potreste essere in tanti in questo momento.\nRiprova tra qualche minuto."
     }
@@ -221,7 +221,23 @@ const it = {
       "USED": "Utilizzato"
     },
     "operationsSection": {
-      "title": "Le tue operazioni"
+      "title": "Le tue operazioni",
+      "onboardingInitiative": "Adesione iniziativa"
+    },
+  },
+  "drawerDetail": {
+    "title": "Dettaglio dell’operazione",
+    "assetAmount": "Importo del bene",
+    "appliedDiscount": "Sconto riconosciuto",
+    "transactionInformation": "Informazioni sulla transazione",
+    "merchant": "Esercente",
+    "date": "Data",
+    "status": "Stato",
+    "idTransaction": "ID transazione",
+    "statusMap": {
+      "AUTHORIZED": "Autorizzata",
+      "CANCELLED": "Cancellata",
+      "REWARDED": "Assegnata"
     }
   }
 }
