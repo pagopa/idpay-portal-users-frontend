f045a09d25bcb686b2b0c9c1625fb781c38e74ff
Francesco Fico <Francesco.Fico@nttdata.com>
2026-08-25 16:15:44 +0200

    fix tests and bg color

--- src/components/PrivacyAndTosLayout/PrivacyAndTosLayout.tsx
+++ src/components/PrivacyAndTosLayout/PrivacyAndTosLayout.tsx
@@ -10,12 +10,12 @@ export const PrivacyAndTosLayout = ({ text, children }: Props) => {
 
   return (
     <>
-      <Box sx={{ width: '100%'}}>
-        <Paper elevation={1} square={true}>
+      <Box sx={{ width: '100%', backgroundColor: '#F5F5F5' }}>
+        <Paper elevation={1} square={true} sx={{ backgroundColor: '#F5F5F5' }}>
           <Box px={4} pt={2} pb={4}>
             <Grid container>
               <Grid size={{ xs: 12 }}>
-                {text ? (
+                {text !== undefined ? (
                   <div
                     className="content"
                     dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(text) }}
