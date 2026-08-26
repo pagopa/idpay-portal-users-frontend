9665c4ea664ed5bca431bb6283f697450ee85c1c
abilello1 <alessio.bilello@emeal.nttdata.com>
2026-08-26 17:05:01 +0200

    add email lowercase normalization

--- src/utils/validateEmail.ts
+++ src/utils/validateEmail.ts
@@ -1,4 +1,6 @@
+export const normalizeEmail = (email: string): string => email.toLowerCase();
+
 export function isValidEmail(email: string): boolean {
-  const emailRegex = /^(?!.*\.\.)[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[A-Za-z0-9-]+(?:\.[A-Za-z0-9-]+)*\.[A-Za-z]{2,}$/;
-  return emailRegex.test(email);
+  const emailRegex = /^(?=.{1,255}$)[A-Za-z0-9]([A-Za-z0-9+_-]*(\.[A-Za-z0-9+_-]+)*)?@[A-Za-z0-9-]+(\.[A-Za-z0-9-]+)*\.[A-Za-z]{2,}$/;
+  return emailRegex.test(normalizeEmail(email));
 }
